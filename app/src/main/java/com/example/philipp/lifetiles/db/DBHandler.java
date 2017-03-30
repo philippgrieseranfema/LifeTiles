8d873fb06d4cada7a0db26fd64fe7b9f0233dc0e
philippgrieseranfema <p.grieser@anfe.ma>
2017-03-31 00:08:20 +0200

    db init

---
+++ app/src/main/java/com/example/philipp/lifetiles/db/DBHandler.java
@@ -0,0 +1,133 @@
+package com.example.philipp.lifetiles.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.philipp.lifetiles.components.Category;
+import com.example.philipp.lifetiles.components.Tile;
+
+public class DBHandler extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "lifetile";
+    private static final int DATABASE_VERSION = 1;
+    private static final String KEY_ID = "id";
+
+    private static final String TABLE_TILES = "tiles";
+    private static final String KEY_TILE_NAME = "name";
+    private static final String KEY_TILE_DESCRIPTION = "description";
+    private static final String KEY_TILE_ICON = "icon";
+
+    private static final String TABLE_CATEGORIES = "categories";
+    private static final String KEY_CATEGORY_NAME = "name";
+    private static final String KEY_CATEGORY_ICON = "icon";
+    private static final String KEY_CATEGORY_ICON_PRESSED = "iconPressed";
+    private static final String KEY_CATEGORY_ICON_CATEGORY = "iconCategory";
+    private static final String KEY_CATEGORY_COLOR = "color";
+
+    private static final String TABLE_MATCH = "match";
+    private static final String KEY_MATCH_CATEGORY_ID = "category";
+    private static final String KEY_MATCH_TILE_ID = "tile";
+
+    public DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // categories
+        String CREATE_CATEGORIES_TABLE = "CREATE TABLE " + TABLE_CATEGORIES + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_CATEGORY_NAME + " TEXT,"
+                + KEY_CATEGORY_ICON + " NUMERIC," + KEY_CATEGORY_ICON_PRESSED + " NUMERIC,"
+                + KEY_CATEGORY_ICON_CATEGORY + " NUMERIC," + KEY_CATEGORY_COLOR + " NUMERIC" + ")";
+        db.execSQL(CREATE_CATEGORIES_TABLE);
+        System.out.println("Created table: " + TABLE_CATEGORIES);
+        // tiles
+        String CREATE_TILES_TABLE = "CREATE TABLE " + TABLE_TILES + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_TILE_NAME + " TEXT,"
+                + KEY_TILE_DESCRIPTION + " TEXT," + KEY_TILE_ICON + " NUMERIC" + ")";
+        db.execSQL(CREATE_TILES_TABLE);
+        System.out.println("Created table: " + TABLE_TILES);
+        // match
+        String CREATE_MATCH_TABLE = "CREATE TABLE " + TABLE_MATCH + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_MATCH_CATEGORY_ID + " NUMERIC,"
+                + KEY_MATCH_TILE_ID + " NUMERIC" + ")";
+        db.execSQL(CREATE_MATCH_TABLE);
+        System.out.println("Created table: " + TABLE_MATCH);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TILES);
+        // Creating tables again
+        onCreate(db);
+    }
+
+    public void addTile(Tile tile) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_TILE_NAME, tile.getName());
+        values.put(KEY_TILE_DESCRIPTION, tile.getDescription());
+        values.put(KEY_TILE_ICON, tile.getIcon());
+        db.insert(TABLE_TILES, null, values);
+        db.close();
+        System.out.println("Added tile: " + tile);
+    }
+
+    public void addCategory(Category category) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_CATEGORY_NAME, category.getName());
+        values.put(KEY_CATEGORY_ICON, category.getIcon());
+        values.put(KEY_CATEGORY_ICON_PRESSED, category.getIconPressed());
+        values.put(KEY_CATEGORY_ICON_CATEGORY, category.getIconCategory());
+        // values.put(KEY_CATEGORY_COLOR, category.getColor()); TODO
+        db.insert(TABLE_CATEGORIES, null, values);
+        db.close();
+        System.out.println("Added category: " + category);
+    }
+
+    public void addMatch(Category category, Tile tile) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_MATCH_CATEGORY_ID, category.getId());
+        values.put(KEY_MATCH_TILE_ID, tile.getId());
+        db.insert(TABLE_MATCH, null, values);
+        db.close();
+        System.out.println("Added match: " + category.getId() + " - " + tile.getId());
+    }
+
+    public Tile getTile(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_TILES, new String[]{KEY_ID,
+                        KEY_TILE_NAME, KEY_TILE_DESCRIPTION, KEY_TILE_ICON}, KEY_ID + "=?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        Tile tile = new Tile(Integer.parseInt(cursor.getString(0)),
+                Integer.parseInt(cursor.getString(3)), cursor.getString(1), cursor.getString(2), 100, 100);
+
+        return tile;
+    }
+
+    public Category getCategory(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_CATEGORIES, new String[]{KEY_ID,
+                        KEY_TILE_NAME, KEY_CATEGORY_ICON, KEY_CATEGORY_ICON_PRESSED, KEY_CATEGORY_ICON_CATEGORY}, KEY_ID + "=?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        Cursor cursor2 = db.query(TABLE_MATCH, new String[]{KEY_ID, // TODO !!!
+                        KEY_MATCH_CATEGORY_ID, KEY_MATCH_TILE_ID}, cursor.getString(0) + "=" + KEY_MATCH_CATEGORY_ID,
+                new String[]{String.valueOf(id)}, null, null, null, null);
+        if (cursor2 != null) {
+            cursor2.moveToFirst();
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/philipp/lifetiles/db/DBInit.java
@@ -0,0 +1,50 @@
+package com.example.philipp.lifetiles.db;
+
+import android.content.Context;
+
+import com.example.philipp.lifetiles.components.Category;
+import com.example.philipp.lifetiles.components.Tile;
+
+import java.io.File;
+
+/**
+ * Created by Philipp on 25.03.2017.
+ */
+
+public class DBInit {
+
+    DBHandler dbHandler;
+
+    public DBInit() {
+        // this class handles first launch of app
+    }
+
+    public boolean isFirstLaunch(Context context) {
+        File dbFile = context.getDatabasePath(DBHandler.DATABASE_NAME);
+        return dbFile.exists();
+    }
+
+    public void initDB(Context context) {
+        this.dbHandler = new DBHandler(context);
+        initTiles();
+    }
+
+    private void initTiles() {
+        // add tiles
+        for (Tile tile : DBFixtures.getAllIconTiles()) {
+            dbHandler.addTile(tile);
+        }
+        // add categories
+        for (Category category : DBFixtures.getCategories()) {
+            dbHandler.addCategory(category);
+        }
+        // add matches
+        for (Category category : DBFixtures.getCategories()) {
+            for (Tile tile : category.getTiles()) {
+                dbHandler.addMatch(category, tile);
+            }
+        }
+        Category category = dbHandler.getCategory(1);
+    }
+
+}
